05ebaa4d8e35f3d19e7764f9eed0a5e502a8fa26
chenzheng <987102818@qq.com>
2018-10-10 10:22:14 +0800

    update 20181010

--- xp.c
+++ xp.c
@@ -10,6 +10,9 @@
 #define T XP_T
 #define BASE (1 << 8) /* base = 256 */
 
+/*
+ * XP_fromint初始化一个XP_T, 使之等于某个特定的unsigned long值
+ */
 unsigned long XP_fromint(int n, T z, unsigned long u)
 {
     int i = 0;
@@ -133,4 +136,47 @@ int XP_product(int n, T z, T x, int y)
         carry /= BASE;
     }
     return carry;
+}
+
+int XP_div(int n, T q, T x, int m, T y, T r, T tmp)
+{
+    int nx = n, my = m;
+
+    n = XP_length(n, x);
+    m = XP_length(m, y);
+    if (m == 1) {
+        // single-digit division
+        if (y[0] == 0)
+            return 0;
+        r[0] = XP_quotient(nx, q, x, y[0]);
+        memset(r + 1, '\0', my - 1);
+    } else if (m > n) {
+        memset(q, '\0', nx);
+        memset(r, x, n);
+        memset(r + n, '\0', my - n);
+    } else {
+        // long division
+        int k;
+        unsigned char* rem = tmp, *dq = tmp + n + 1;
+        assert(2 <= m && m <= n);
+        memcpy(rem, x, n);
+        rem[n] = 0;
+        for (k = n - m; k >= 0; k--) {
+            int qk;
+            // compute qk, dq <- y*qk
+            {
+                int i;
+                assert(2 <= m && m <= k+m && k+m <= n);
+                // qk <- y[m-2..m-1]/rem[k+m-2..k+m]
+                {
+                    int km = k + m;
+                    unsigned long y2 = y[m-1] * BASE + y[m-2];
+                    unsigned long r3 = rem[km] * (BASE * BASE) + rem[km-1] * BASE + rem[km-2];
+                    qk = r3/y2;
+                    if (qk >= BASE)
+                        qk = BASE - 1;
+                }
+            }
+        }
+    }
 }

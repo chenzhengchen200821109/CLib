fa283e44f8ef289ce0a2ef569d8b05c3b383a66c
Joey Chen <987102818@qq.com>
2018-12-03 14:34:18 +0800

    update 20181203

--- thread.c
+++ thread.c
@@ -7,173 +7,255 @@
 #include <string.h>
 #include <signal.h>
 #include <sys/time.h>
-#include "assert_v.h"
-#include "except_v.h"
+#include <assert.h>
+//#include "assert_v.h"
+//#include "except_v.h"
 #include "mem.h"
 #include "thread.h"
+#include "error.h"
 //#include "sem.h"
 
-static T current;
-static int nthreads;
-static struct Thread_T root;
-static T join0; // join0队列只包含一个线程, 来等待所有其他线程结束
-static T freelist;
-static int critical;
-
-void _MONITOR(void) {}
-extern void _ENDMONITOR(void);
-
-#define T Thread_T
 #define isempty(q) ((q) == NULL)
 
-struct T {
-    unsigned long *sp;
-    T link;
-    T *inqueue;
-    T handle;
-    Except_Frame* estack;
+typedef int (*tcontext_handler_t)(void* s1, void* s2);
+
+enum
+{
+    eip = 0,
+    ebx,
+    ecx,
+    edx,
+    edi,
+    esi,
+    ebp,
+    esp
+};
+
+struct TContext
+{
+    int buffer[8];
+    int ss_size;
+    char* ss_sp;
+};
+
+typedef struct parameter
+{
+    const void* thread;
+    const void* arg;
+} parameter_t;
+
+/* thread control block */
+struct Thread {
+    //unsigned long *sp; /* must be first */
+    void* stack_esp;
+    void* stack_ebp;
+    int (*startup)(void *);
+    void* arg;
+    struct TContext context;
+    struct Thread* link;
+    struct Thread** inqueue; /* pointer to the queue variable - ready */
+    struct Thread* handle; /* t identifies an existing thread only if t is equal to t->handle */   
+    //Except_Frame* estack;
     int code;
-    T join;
-    T next;
+    struct Thread* join;
+    struct Thread* next;
     int alerted;
 };
 
-static T ready = NULL;
-/* prototypes */
-extern void _swtch(T from, T to);
+static struct Thread* current; /* the thread that currently holds the processor */
+static int nthreads; /* the number of of existing threads */
+static struct Thread root; /* main thread */
+static struct Thread* join0; // join0队列只包含一个线程, 来等待所有其他线程结束
+static struct Thread* freelist;
+static int critical;
 
-const Except_T Thread_Alerted = { "Thread alerted" };
+/*
+ * The vacuous function _MONITOR and the external function _ENDMONITOR
+ * are used only for their address. These addresses encompass critical
+ * sections - thread code that must not be interrupted.
+ */
+void _MONITOR(void) {}
+extern void _ENDMONITOR(void);
+
+/* ready queue, which holds running threads that do not have processors */
+static struct Thread* ready = NULL;
+/*
+ * _swtch is like setjmp and longjmp. When thread A calls _swtch, control
+ * transfers to thread B. When B calls _swtch to resume A, A's call to 
+ * _swtch returns.
+ */
+extern void _swtch(struct TContext* from, struct TContext* to);
+
+int TContext_init(struct TContext* context)
+{
+    memset(context, 0, sizeof(*context));
+}
+
+int TContext_make(struct TContext* context, tcontext_handler_t pfn, const void* thread, const void* arg)
+{
+    char* sp = context->ss_sp + context->ss_size - sizeof(parameter_t);
+    sp = (char *)((unsigned int)sp & ~16L);
+
+    parameter_t* param = (parameter_t *)sp;
+    param->thread = thread;
+    param->arg = arg;
+
+    memset(context->buffer, 0, sizeof(context->buffer));
+    context->buffer[esp] = (int)((char *)sp - sizeof(void *));
+    context->buffer[eip] = (int)(char *)pfn;
+
+    return 0;
+
+}
+
+//const Except_T Thread_Alerted = { "Thread alerted" };
 
 /*
  * put的参数包括队列变量的地址, 因而可以修改该变量。
+ * put(t, &ready) appends t to the ready queue.
  */
-static void put(T t, T* q)
+static void put(struct Thread* t, struct Thread** queue)
 {
     assert(t);
     assert(t->inqueue == NULL && t->link == NULL);
-    if (*q) { // queue is not empty
-        t->link = (*q)->link;
-        (*q)->link = t;
+    if (*queue) { // queue is not empty
+        t->link = (*queue)->link;
+        (*queue)->link = t;
     } else // queue is empty
         t->link = t;
-    *q = t;
-    t->inqueue = q;
+    *queue = t;
+    t->inqueue = queue;
 }
 
 /*
  * get从一个给定队列中移除第一个元素
+ * get() removes the first element from a given queue.
  */
-static T get(T* q)
+static struct Thread* get(struct Thread** queue)
 {
-    T t;
+    struct Thread* t;
 
-    assert(!isempty(*q));
-    t = (*q)->link;
-    if (t == *q) // only one thread
-        *q = NULL;
+    assert(!isempty(*queue));
+    t = (*queue)->link;
+    if (t == *queue) // only one thread
+        *queue = NULL;
     else
-        (*q)->link = t->link;
-    assert(t->inqueue == q);
+        (*queue)->link = t->link;
+    assert(t->inqueue == queue);
     t->link = NULL;
     t->inqueue = NULL;
     return t;
 }
 
-static void delete(T t, T* q)
+static void delete(struct Thread* t, struct Thread** queue)
 {
-    T p;
+    struct Thread* p;
 
-    assert(t->link && t->inqueue == q);
-    assert(!isempty(*q));
-    for (p = *q; p->link != t; p = p->link)
+    assert(t->link && t->inqueue == queue);
+    assert(!isempty(*queue));
+    for (p = *queue; p->link != t; p = p->link)
         ;
     if (p == t)
-        *q = NULL;
+        *queue = NULL;
     else {
         p->link = t->link;
-        if (*q == t)
-            *q = p;
+        if (*queue == t)
+            *queue = p;
     }
     t->link = NULL;
     t->inqueue = NULL;
 }
 
-static void testalert(void)
+static void run(void)
+{
+    struct Thread* t = current;
+
+    current = get(&ready);
+    //t->estack = Except_stack;
+    //Except_stack = current->estack;
+    _swtch(&(t->context), &(current->context));
+}
+
+
+/* static void testalert(void)
 {
     if (current->alerted) {
         current->alerted = 0;
-        RAISE(Thread_Alerted);
+        //RAISE(Thread_Alerted);
     }
-}
+} */
 
+/*
+ * release all threads.
+ */
 static void release(void)
 {
-    T t;
+    struct Thread* t;
     do {
         critical++;
         while ((t = freelist) != NULL) {
             freelist = t->next;
+            FREE(t->stack_ebp);
             FREE(t);
         }
         critical--;
     } while(0);
 }
 
-static int interrupt(int sig, int code, struct sigcontext *scp)
-{
-    if (critical || scp->sc_pc >= (unsigned long)_MONITOR && scp->sc_pc <= (unsigned long)_ENDMONITOR)
-        return 0;
-    put(current, &ready);
-    sigsetmask(scp->sc_mask);
-    run();
-    return 0;
-}
+//static int interrupt(int sig, int code, struct sigcontext *scp)
+//{
+//    if (critical || scp->sc_pc >= (unsigned long)_MONITOR && scp->sc_pc <= (unsigned long)_ENDMONITOR)
+//        return 0;
+//    put(current, &ready);
+//    sigsetmask(scp->sc_mask);
+//    run();
+//    return 0;
+//}
 
-int Thread_init(int preempt, ...)
+/* 
+ * Thread_init() must be called only once.
+ */
+int Thread_init(int preempt)
 {
     assert(preempt == 0 || preempt == 1);
     assert(current == NULL);
     root.handle = &root;
     current = &root;
     nthreads = 1;
-    if (preempt) {
+    //if (preempt) {
         // initialize preemptive scheduling
-        {
-            struct sigaction sa;
-            memset(&sa, '\0', sizeof(sa));
-            sa.sa_handler = (void (*)())interrupt;
-            if (sigaction(SIGVTALRM, &sa, NULL) < 0)
-                return 0;
-        }
-        {
-            struct itimerval it;
-            it.it_value.tv_sec = 0;
-            it.it_value.tv_usec = 50;
-            it.it_interval.tv_sec = 0;
-            it.it_interval.tv_usec = 50;
-            if (setitimer(ITIMER_VIRTUAL, &it, NULL) < 0)
-                return 0;
-        }
-    }
-    return 1;
+        //{
+        //    struct sigaction sa;
+        //    memset(&sa, '\0', sizeof(sa));
+        //    sa.sa_handler = (void (*)())interrupt;
+        //    if (sigaction(SIGVTALRM, &sa, NULL) < 0)
+        //        return 0;
+        //}
+        //{
+        //    struct itimerval it;
+        //    it.it_value.tv_sec = 0;
+        //    it.it_value.tv_usec = 50;
+        //    it.it_interval.tv_sec = 0;
+        //    it.it_interval.tv_usec = 50;
+        //    if (setitimer(ITIMER_VIRTUAL, &it, NULL) < 0)
+        //        return 0;
+        //}
+    //}
+    return 0;
 }
 
-T Thread_self(void)
+struct Thread* Thread_self(void)
 {
     assert(current);
     return current;
 }
 
-static void run(void)
-{
-    T t = current;
 
-    current = get(&ready);
-    t->estack = Except_stack;
-    Except_stack = current->estack;
-    _swtch(t, current);
-}
-
+/*
+ * All of the Thread functions that can cause a context
+ * switch call run, and they put the current thread on
+ * ready or another appropriate queue before calling run.
+ */
 void Thread_pause(void)
 {
     assert(current);
@@ -181,31 +263,42 @@ void Thread_pause(void)
     run();
 }
 
-int Thread_join(T t)
+/*
+ * Thread_join() waits for thread t to terminate and returns
+ * t's exit code.
+ */
+int Thread_join(struct Thread* t)
 {
     assert(current && t != current);
-    testalert();
+    //testalert();
     if (t) {
         // wait for thread to terminate
         if (t->handle == t) {
             put(current, &t->join);
             run();
-            testalert();
+            //testalert();
             return current->code;
         } else
             return -1;
     } else {
-        // wait for all threads to terminate
+        // wait for all threads to terminate if t is NULL
         assert(isempty(join0));
         if (nthreads > 1) {
             put(current, &join0);
             run();
-            testalert();
+            //testalert();
         }
         return 0;
     }
 }
 
+/*
+ * Thread_exit() has numerous jobs to do: It must deallocate the 
+ * resources associated with the calling thread, resume the threads
+ * waiting for the calling thread to terminated an arrange for them
+ * to get the exit code, and check whether the calling thread is the
+ * second to last or last thread in the system.
+ */
 void Thread_exit(int code)
 {
     assert(current);
@@ -217,7 +310,7 @@ void Thread_exit(int code)
     current->handle = NULL;
     // resume threads waiting for current's termination
     while (!isempty(current->join)) {
-        T t = get(&current->join);
+        struct Thread* t = get(&current->join);
         t->code = code;
         put(t, &ready);
     }
@@ -235,7 +328,7 @@ void Thread_exit(int code)
 /*
  * Thread_alert将一线程线程标记"警报-待决"状态
  */
-void Thread_alert(T t)
+void Thread_alert(struct Thread* t)
 {
     assert(current);
     assert(t && t->handle == t);
@@ -246,45 +339,62 @@ void Thread_alert(T t)
     }
 }
 
-T Thread_new(int apply(void*), void* args, int nbytes, ...)
+static int Thread_startup(struct Thread* t, void* arg)
 {
-    T t;
+    if (t->startup)
+        t->startup(t->arg);
+    Thread_exit(0);
+}
+
+/*
+ * Thread_new() has four tasks: allocate the resources for a
+ * new thread, initialize the new thread's state so that it can
+ * be started by a return from _swtch, increment nthreads, and 
+ * append the new thread to ready.
+ */
+struct Thread* Thread_new(int apply(void*), void* args)
+{
+    struct Thread* t;
+    int stacksize;
+    void* stack_top;
 
     assert(current);
     assert(apply);
-    assert(args && nbytes >= 0 || args == NULL);
-    if (args == NULL)
-        nbytes = 0;
     // allocate resources for a new thread
     {
-        int stacksize = (16 * 1024 + sizeof(*t) + nbytes + 15) & ~15;
+        stacksize = (16 * 1024);
         release();
         do {
             critical++;
             //TRY
-            t = ALLOC(stacksize);
+            t = ALLOC(sizeof(*t));
             memset(t, '\0', sizeof(*t));
+            stack_top = ALLOC(stacksize);
             //EXCEPT(Mem_Failed);
-            t = NULL;
+            //t = NULL;
             // END_TRY
             critical--;
         } while (0);
-        if (t == NULL)
-            RAISE(Thread_Failed);
-        // initialize t's stack pointer
-        t->sp = (void *)((char *)t + stacksize);
-        while (((unsigned long)t->sp) & 15)
-            t->sp--;
+        if (t == NULL || stack_top == NULL)
+            err_exit(errno, "ALLOC error", __FILE__, __LINE__);
+            //RAISE(Thread_Failed);
+        /*
+         * initialize t's stack pointer so that it's aligned on a 16-byte
+         * boundary, which accommodates most platforms.
+         */
     }
-    t->handle = t;
-    if (nbytes > 0) {
-        t->sp -= ((nbytes + 15U) & ~15) / sizeof(*t->sp);
-        do {
-            critical++;
-            memcpy(t->sp, args, nbytes);
-            critical--;
-        } while (0);
-        args = t->sp;
-    }
+    t->stack_esp = (char *)stack_top + stacksize;
+    t->stack_ebp = (char *)stack_top;
+    t->startup = apply;
+    t->arg = args;
+    nthreads++;
+
+    TContext_init(&(t->context));
+    t->context.ss_size = stacksize;
+    t->context.ss_sp = t->stack_ebp;
+    TContext_make(&(t->context), (tcontext_handler_t)Thread_startup, t, args);
+    put(t, &ready);
+
+    return t;
 }
 
--- thread.h
+++ thread.h
@@ -2,20 +2,18 @@
 // Created by chenzheng on 06/12/2017.
 //
 
-#ifndef CLIB_THREAD_H
-#define CLIB_THREAD_H
+#ifndef _THREAD_H
+#define _THREAD_H
 
-#endif //CLIB_THREAD_H
-
-#include "except_v.h"
-#define T Thread_T
+//#include "except_v.h"
+#define T Thread
 typedef struct T *T;
 
-extern const Except_T Thread_Failed;
-extern const Except_T Thread_Alerted;
+//extern const Except_T Thread_Failed;
+//extern const Except_T Thread_Alerted;
 
-extern int Thread_init(int preempt, ...);
-extern T Thread_new(int apply(void *), void *args, int nbytes, ...);
+extern int Thread_init(int preempt);
+extern T Thread_new(int apply(void *), void *args);
 /*
  * 线程调用Thread_exit(code)函数后, 将结束该线程的执行。此后, (借助于Thread_join)
  * 等待该线程结束的线程将恢复执行, code的值将作为调用Thread_join的结果返回给这些恢复
@@ -26,6 +24,7 @@ extern void Thread_alert(T t);
 extern T Thread_self(void);
 extern int Thread_join(T t);
 extern void Thread_pause(void);
+extern void Thread_start();
 
-#undef
-#endif
+#undef T
+#endif // _THREAD_H
